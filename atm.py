72d43ffc386156dceb3572b9a138e49977443688
lowercaselife <dansonkalaghe@gmail.com>
2022-03-15 16:46:05 +0300

    revised

--- atm.py
+++ atm.py
@@ -49,22 +49,28 @@ def transaction(account):
         a dictionary with keys ('person', 'pin', 'main_account', 'dollar_account')
         Return the modified account. """
     while True:
-        # reciept = input('Do you want a reciept(y or n: ')
-        # if reciept == "y":
+        
         # show account status
-        print(" status: Hello {} , you have Ksh. {} in main account, {:.2f} dollars in your dollar account.".format(
+        print("Account Status: Hello {} , you have Ksh. {:.2f} in main account, {:.2f} dollars in your dollar account.".format(
             account['person'], account['main_account'], account['dollar_account']))
-        # else:
-        #     print("Thanks for banking with us.")
-
+        
         # do one transaction
-        what = input(' transaction (deposit, withdraw, transfer, end) ? ')
+      
+        what = input('\n****Menu****\n 1. Deposit\n 2. Withdraw\n 3. Transfer\n 4. Exit\n************\n :')
 
-        if what[0] == 'e':    # end
+
+        if what[0] == '4':    # end
             return account
-
-        elif what[0] == 'w':  # withdraw
-            which = input('  account (main_account, dollar_account) ? ')
+        elif what[0] == '2':  # withdraw
+            enter_account = input('Choose account (1. Main account, 2. Dollar account) ? ')
+            if enter_account == '1':
+                which = 'main_account'
+            elif enter_account == '2':
+                which = 'dollar_account'
+            else:
+                 print("Enter a valid account.") 
+                 break
+            
             amount = float(input('  amount (xxx.xx) ? '))
             if amount > account[which]:
                 print("Not enough money in account")
@@ -76,19 +82,26 @@ def transaction(account):
                     print("********************************")
                     print("    TRANSACTION SUCCESSFULL     ")
                     print(" Account holder: {}".format(account['person']))
-                    print(" Account holder: {}".format(account['main_account']))
+                    print(" Main Account: {}".format(account['main_account']))
                     print(" Dollar account balance: {:.2f}".format(account['dollar_account']))
                     print("********************************")
                     print('')
                 else:
                     print("Thank you for banking with us!!")
                     print('')
-        elif what[0] == 'd':  # deposit
-            which = input('  account (main_account, dollar_account) ? ')
+        elif what[0] == '1':  # deposit
+            enter_account = input('Choose account (1. Main account, 2. Dollar account) ? ')
+            if enter_account == '1':
+                which = 'main_account'
+            elif enter_account == '2':
+                which = 'dollar_account'
+            else:
+                 print("Enter a valid account.") 
+                 break
             amount = float(input('  amount (xxx.xx) ? '))
             account[which] = account[which] + amount
 
-        elif what[0] == 't':  # transfer
+        elif what[0] == '3':  # transfer
             which = input(' 1 (main account to dollar account) or 2 (dollar account to main account) ? ')
             amount = float(input('  amount (xxx.xx) ? '))
             if which == '1':
